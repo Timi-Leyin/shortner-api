bc89954a3adc7f473a9c9592ee3de86164c7f092
OriginalTimi <timileyinoyelekan11@gmail.com>
2024-01-25 15:29:20 +0100

    fix: test response

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -4,13 +4,13 @@ import request from "supertest";
 import app from "../src/app";
 import mainConfig from "../src/config/main";
 
-const LINK_ID = "3bad17";
+const LINK_ID = "QACq0CP";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIzYmFkMTc0ZC0zNzc3LTQzMDYtODBhNC1hMWE2MGY2OWI1ZGUiLCJpYXQiOjE3MDM0NTQzNzgsImV4cCI6MTcwMzU0MDc3OH0.x5dpdhav6ft2FP8f_9NjhQCVaTK9QYOEKmkuQMtjp7Y";
 describe("GET Endpoints", () => {
   it("should return a 200 status code", async () => {
     const res = await request(app).get(`/${LINK_ID}`);
-    expect(res.status).toBe(200);
+    expect(res.status).toBe(302);
   });
 });
 
@@ -20,5 +20,5 @@ describe("Created Link Endpoints", () => {
       url: "https://google.com/09121msd/22",
     });
     expect(res.statusCode).toEqual(201);
-  });
+  }, 100000);
 });
